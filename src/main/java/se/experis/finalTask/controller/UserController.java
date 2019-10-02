9068b9d6f54354f2547fd4718168ca5ca4827558
Elliot643 <elliot643@hotmail.com>
2019-10-02 10:43:36 +0200

    First Commit

---
+++ src/main/java/se/experis/finalTask/controller/UserController.java
@@ -0,0 +1,8 @@
+package se.experis.finalTask.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
---
+++ src/main/java/se/experis/finalTask/model/User.java
@@ -0,0 +1,9 @@
+package se.experis.finalTask.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+
+}
